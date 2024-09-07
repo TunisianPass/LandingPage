a5a5ece2128cb7654b03d1943d0d0f38b860b2ed
Hedi Fourati <hfourati2007@gmail.com>
2024-09-07 18:06:39 +0100

    test

--- components/SimpleBanner.js
+++ components/SimpleBanner.js
@@ -5,7 +5,7 @@ const SimpleBanner = () => {
         className={`absolute left-0 right-0 top-0 z-50 transition-all duration-300 ease-in-out`}
       >
         <div className="flex w-full items-center justify-center gap-x-6 bg-black px-6 py-3 sm:px-3.5">
-          <div className="flex items-center text-sm font-medium leading-6 text-[#ff7900] bg-gradient-to-r  inline-block text-transparent bg-clip-text gap-5">
+          <div className="flex items-center text-sm font-medium leading-6 text-[#ff7900]   bg-clip-text gap-5">
             <p>{`Propulsé par Orange Tunisie`}</p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className=" w-5 h-5">
 
